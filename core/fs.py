fb3042c5ae6f21f0544e4af154bab476e8a329e9
Gonzalo Correa <gcorrea.q@gmail.com>
2021-07-17 23:32:55 -0700

    More readable code for File class
    Instead of dynamically figuring out how to calculate fields, this
    creates a set of new properties that each one take care of a single job.
    This not only helps with code readability, but also helps on
    encapsulating the logic better.
    
    In most places I tried to keep the logic as it was, so all tests should
    still pass. Still, some changes are included that take advantage of some
    Python good practices
    
    - Use context managers to open files, in that way, any errors while
      reading the files or updating the hash objects will automatically
      close the file objects.
    - Instead of setting properties on the fly, this defines properties as
      custom methods that take care of calculating values or caching them
      internally in the object

--- core/fs.py
+++ core/fs.py
@@ -13,6 +13,7 @@
 
 import hashlib
 import logging
+from operator import attrgetter
 
 from hscommon.util import nonone, get_file_ext
 
@@ -82,29 +83,17 @@ class File:
     # Slots for File make us save quite a bit of memory. In a memory test I've made with a lot of
     # files, I saved 35% memory usage with "unread" files (no _read_info() call) and gains become
     # even greater when we take into account read attributes (70%!). Yeah, it's worth it.
-    __slots__ = ("path", "is_ref", "words") + tuple(INITIAL_INFO.keys())
+    __slots__ = ("path", "is_ref", "words", "_size", "_mtime", "_md5", "_md5partial")
 
     def __init__(self, path):
         self.path = path
-        for attrname in self.INITIAL_INFO:
-            setattr(self, attrname, NOT_SET)
+        self._size = NOT_SET
+        self._mtime = NOT_SET
+        self._md5 = NOT_SET
+        self._md5partial = NOT_SET
 
     def __repr__(self):
         return "<{} {}>".format(self.__class__.__name__, str(self.path))
-
-    def __getattribute__(self, attrname):
-        result = object.__getattribute__(self, attrname)
-        if result is NOT_SET:
-            try:
-                self._read_info(attrname)
-            except Exception as e:
-                logging.warning(
-                    "An error '%s' was raised while decoding '%s'", e, repr(self.path)
-                )
-            result = object.__getattribute__(self, attrname)
-            if result is NOT_SET:
-                result = self.INITIAL_INFO[attrname]
-        return result
 
     # This offset is where we should start reading the file to get a partial md5
     # For audio file, it should be where audio data starts
@@ -112,37 +101,12 @@ class File:
         return (0x4000, 0x4000)  # 16Kb
 
     def _read_info(self, field):
-        if field in ("size", "mtime"):
-            stats = self.path.stat()
-            self.size = nonone(stats.st_size, 0)
-            self.mtime = nonone(stats.st_mtime, 0)
-        elif field == "md5partial":
-            try:
-                fp = self.path.open("rb")
-                offset, size = self._get_md5partial_offset_and_size()
-                fp.seek(offset)
-                partialdata = fp.read(size)
-                md5 = hashlib.md5(partialdata)
-                self.md5partial = md5.digest()
-                fp.close()
-            except Exception:
-                pass
-        elif field == "md5":
-            try:
-                fp = self.path.open("rb")
-                md5 = hashlib.md5()
-                # The goal here is to not run out of memory on really big files. However, the chunk
-                # size has to be large enough so that the python loop isn't too costly in terms of
-                # CPU.
-                CHUNK_SIZE = 1024 * 1024  # 1 mb
-                filedata = fp.read(CHUNK_SIZE)
-                while filedata:
-                    md5.update(filedata)
-                    filedata = fp.read(CHUNK_SIZE)
-                self.md5 = md5.digest()
-                fp.close()
-            except Exception:
-                pass
+        try:
+            return getattr(self, field)
+        except Exception as e:
+            logging.warning(
+                "An error '%s' was raised while decoding '%r'", e, self.path
+            )
 
     def _read_all_info(self, attrnames=None):
         """Cache all possible info.
@@ -193,6 +157,71 @@ class File:
     def folder_path(self):
         return self.path.parent()
 
+    @property
+    def md5partial(self):
+        if self._md5partial is NOT_SET:
+            md5_hash = hashlib.md5()
+            try:
+                with self.path.open("rb") as file_obj:
+                    offset, size = self._get_md5partial_offset_and_size()
+                    file_obj.seek(offset)
+                    partialdata = file_obj.read(size)
+                    md5_hash.update(partialdata)
+            except Exception:
+                logging.exception(
+                    "Error while calculating partial MD5 for %s", self.path
+                )
+                return ""
+            else:
+                self._md5partial = md5_hash.digest()
+
+        return self._md5partial
+
+    @property
+    def md5(self):
+        if self._md5 is NOT_SET:
+            md5_hash = hashlib.md5()
+            try:
+                with self.path.open("rb") as fp:
+                    # The goal here is to not run out of memory on really big files. However, the chunk
+                    # size has to be large enough so that the python loop isn't too costly in terms of
+                    # CPU.
+                    CHUNK_SIZE = 1024 * 1024  # 1 mb
+                    filedata = fp.read(CHUNK_SIZE)
+                    while filedata:
+                        md5_hash.update(filedata)
+                        filedata = fp.read(CHUNK_SIZE)
+            except Exception:
+                logging.exception("Error while calculating MD5 for %s", self.path)
+                return ""
+            else:
+                self._md5 = md5_hash.digest()
+        return self._md5
+
+    @property
+    def mtime(self):
+        self._stat_path()
+        if self._mtime is NOT_SET:
+            return 0
+        return self._mtime
+
+    @property
+    def size(self):
+        self._stat_path()
+        if self._size is NOT_SET:
+            return 0
+        return self._size
+
+    def _stat_path(self):
+        if self._mtime is NOT_SET or self._size is NOT_SET:
+            try:
+                stats = self.path.stat()
+            except Exception:
+                logging.warning("Error while reading stat of path %r", self.path)
+            else:
+                self._size = nonone(stats.st_size, 0)
+                self._mtime = nonone(stats.st_mtime, 0)
+
 
 class Folder(File):
     """A wrapper around a folder path.
@@ -203,33 +232,13 @@ class Folder(File):
     __slots__ = File.__slots__ + ("_subfolders",)
 
     def __init__(self, path):
-        File.__init__(self, path)
+        super().__init__(path)
         self._subfolders = None
 
     def _all_items(self):
         folders = self.subfolders
         files = get_files(self.path)
         return folders + files
-
-    def _read_info(self, field):
-        if field in {"size", "mtime"}:
-            size = sum((f.size for f in self._all_items()), 0)
-            self.size = size
-            stats = self.path.stat()
-            self.mtime = nonone(stats.st_mtime, 0)
-        elif field in {"md5", "md5partial"}:
-            # What's sensitive here is that we must make sure that subfiles'
-            # md5 are always added up in the same order, but we also want a
-            # different md5 if a file gets moved in a different subdirectory.
-            def get_dir_md5_concat():
-                items = self._all_items()
-                items.sort(key=lambda f: f.path)
-                md5s = [getattr(f, field) for f in items]
-                return b"".join(md5s)
-
-            md5 = hashlib.md5(get_dir_md5_concat())
-            digest = md5.digest()
-            setattr(self, field, digest)
 
     @property
     def subfolders(self):
@@ -243,6 +252,44 @@ class Folder(File):
     @classmethod
     def can_handle(cls, path):
         return not path.islink() and path.isdir()
+
+    @property
+    def md5(self):
+        if self._md5 is NOT_SET:
+            self._md5 = self._calculate_md5("md5")
+        return self._md5
+
+    @property
+    def md5partial(self):
+        if self._md5partial is NOT_SET:
+            self._md5partial = self._calculate_md5("md5partial")
+        return self._md5partial
+
+    @property
+    def size(self):
+        if self._size is NOT_SET:
+            self._size = sum((f.size for f in self._all_items()), 0)
+        return self._size
+
+    @property
+    def mtime(self):
+        if self._mtime is NOT_SET:
+            stats = self.path.stat()
+            self._mtime = nonone(stats.st_mtime, 0)
+        return self._mtime
+
+    def _calculate_md5(self, field):
+        # What's sensitive here is that we must make sure that subfiles'
+        # md5 are always added up in the same order, but we also want a
+        # different md5 if a file gets moved in a different subdirectory.
+        def get_dir_md5_concat():
+            items = self._all_items()
+            items.sort(key=attrgetter("path"))
+            md5s = (getattr(f, field) for f in items)
+            return b"".join(md5s)
+
+        md5 = hashlib.md5(get_dir_md5_concat())
+        return md5.digest()
 
 
 def get_file(path, fileclasses=[File]):
